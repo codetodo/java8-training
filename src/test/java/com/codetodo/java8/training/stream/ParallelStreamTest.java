d118bd605f5cedcb28982e5812bc7ec131de4667
codetodo <codetodo@outlook.com>
2017-06-19 15:35:04 +0200

    adding comment

--- src/test/java/com/codetodo/java8/training/stream/ParallelStreamTest.java
+++ src/test/java/com/codetodo/java8/training/stream/ParallelStreamTest.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
